b2264ca7464153afa2263e29afe1396d401cb8c7
CoolShishir <jainshishir18@gmail.com>
2016-12-13 00:25:38 +0530

    simple app

--- server.js
+++ server.js
@@ -4,17 +4,6 @@ var fs = require("fs");
 var path = require("path");
 var bodyParser = require("body-parser");
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   next();
-// });
-
-
-
-
-
 app.use(function(req, res, next) {
     if (req.headers.origin) {
         res.header('Access-Control-Allow-Origin', '*')
@@ -25,17 +14,7 @@ app.use(function(req, res, next) {
     next()
 })
 
-
-
-
-
 app.use(bodyParser());
-
-
-// app.get('*',function(req,res){
-//   res.sendFile(path.join(__dirname+'/index.html'));
-// //  __dirname : It will resolve to your project folder.
-// });
 
 app.get('/order/:id', function (req, res) {
    fs.readFile( __dirname + "/" + "sample-order-dump.json", 'utf8', function (err, data) {
@@ -58,7 +37,7 @@ app.put('/order/:id', function (req, res) {
         console.log(req.body);
         for (i = 0, len = ordersList.length; i < len; i++) {
           if ( req.params.id  == ordersList[i].orderId ) {
-          ordersList[i].state = req.body["state"];
+          ordersList[i]["state"] = req.body["state"];
           break;
         }
       }
@@ -69,10 +48,23 @@ app.put('/order/:id', function (req, res) {
     });  
 });
 
-app.get('/orders', function (req, res) {
+app.get('/orders/', function (req, res) {
    fs.readFile( __dirname + "/" + "sample-order-dump.json", 'utf8', function (err, data) {
        console.log(__dirname);
-       res.end( data ); 
+       page = req.query.pageNo
+       ordersList = JSON.parse( data );
+       j = 0;
+       offset = 10;
+       console.log(page);
+       pageData = "[";
+       for (i = offset*page, len = i+offset; i < len && i < ordersList.length ; i++ , j++) {
+          pageData = pageData +  JSON.stringify(ordersList[i]) + ',';
+        }
+        // get rid of extra coma at the ends
+        pageData = pageData.substr(0, pageData.length - 1)
+        pageData = pageData + ']';
+        console.log(pageData);
+       res.end( pageData ); 
    });
 });
 
@@ -81,6 +73,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port);
+  console.log("app listening at http://%s:%s", host, port);
 
 });
